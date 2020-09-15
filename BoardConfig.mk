6ca70c184dbd2762c8771248fc8dd384ec0490f3
Abhishek <61415942+abhi9960@users.noreply.github.com>
2020-09-15 19:22:06 +0530

    Shrp info side changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,8 +146,8 @@ SHRP_PATH := device/realme/RMX1925
 SHRP_MAINTAINER := Abhishek
 SHRP_DEVICE_CODE := RMX1925
 ##SHRP_EDL_MODE := 1
-SHRP_STATUSBAR_RIGHT_PADDING := 40
-SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_STATUSBAR_RIGHT_PADDING := 48
+SHRP_STATUSBAR_LEFT_PADDING := 48
 SHRP_NOTCH := true
 
 #for dark theme by default 
@@ -159,7 +159,7 @@ SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usbotg
 SHRP_REC := /dev/block/bootdevice/by-name/recovery
 SHRP_REC_TYPE := Treble
-SHRP_DEVICE_TYPE := A
+SHRP_DEVICE_TYPE := A_Only
 SHRP_FLASH := 1
 SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
 SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
