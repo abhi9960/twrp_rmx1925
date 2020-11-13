062acc4b56a3dfcd2f215cffd2d67d904d3dad42
Abhishek Raut <61415942+abhi9960@users.noreply.github.com>
2020-11-13 22:52:31 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -42,7 +42,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export OF_STATUS_INDENT_LEFT="48"
         export OF_STATUS_INDENT_RIGHT="48"
         export OF_SUPPORT_OZIP_DECRYPTION=1
-
+	export OF_CLOCK_POS=1
         export OF_STATUS_H=100
         export TW_DEVICE_VERSION="R11.0"
 	export FOX_ADVANCED_SECURITY=1
@@ -50,21 +50,22 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_VERSION=R11.0_0
         export FOX_R11=1
 	export FOX_DELETE_AROMAFM=1
-	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+	export OF_USE_NEW_MAGISKBOOT=1
 	#export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export OF_TARGET_DEVICES="RMX1925,RMX1911,RMX2030"
-
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_ZIP_BINARY=1
 	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_USE_NEW_MAGISKBOOT=1
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export FOX_QUICK_BACKUP_LIST="/data;/boot;/vendor_image;/system_image"
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+
 
         # use system (ROM) fingerprint where available
         export OF_USE_SYSTEM_FINGERPRINT=1
@@ -73,7 +74,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
         export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
 
-       
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
   	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
